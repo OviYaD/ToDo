6a270908d6352eb83078028208aa3cde64512d0e
oviyad <oviyad.19cse@kongu.edu>
2022-08-07 16:50:21 +0530

    login and register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import TodoList from './components/TodoList'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/todolist" exact element={<TodoList />}></Route>
+          <Route path="/login" exact element={<Login />}></Route>
+          <Route path="/register" exact element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
